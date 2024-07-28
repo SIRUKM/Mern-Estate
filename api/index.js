74ba078d2c2a57fb41a97ef34a9b9aaf9a3e3665
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-07-28 11:12:52 +0530

    chore: Update npm dependencies and add MongoDB connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log("error connecting to mongodb", err);
+  });
 
 const app = express();
-app.listen(3000, () =>{
-    console.log('server is running in port 3000!!')
-})
+app.listen(3000, () => {
+  console.log("server is running in 3000!!");
+});
