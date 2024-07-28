526da8f546e2d5f02d01aefcbec7c48ba1261aaf
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-07-28 14:06:52 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -13,6 +14,10 @@ mongoose
   });
 
 const app = express();
+
+
 app.listen(3000, () => {
   console.log("server is running in 3000!!");
 });
+
+app.use('api/user', userRouter)
---
+++ api/routes/user.route.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const router = express.Router()
+router.get('/test',(req, res)=>{
+    res.json({
+        message:"Hello world from user route"
+    })
+})
+
+export default router
