c11cc0ed0cde7bbba852c9db14b1d036fd8d8d46
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-08-01 15:33:26 +0530

    feat: Add test API route and controller for user

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
+
+console.log(process.env.MONGO);
 
 mongoose
   .connect(process.env.MONGO)
@@ -14,10 +18,22 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-app.listen(3000, () => {
-  console.log("server is running in 3000!!");
+// 404 Not Found Middleware
+app.use((req, res, next) => {
+  res.status(404).json({ message: "404 Not Found" });
 });
 
-app.use('api/user', userRouter)
+// General Error Handling Middleware (optional)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000!!");
+});
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,7 @@
 import express from 'express'
+import { test } from '../controllers/user.controller.js'
 
 const router = express.Router()
-router.get('/test',(req, res)=>{
-    res.json({
-        message:"Hello world from user route"
-    })
-})
+router.get('/test', test)
 
-export default router
+export default router;
